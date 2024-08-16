e71f960614ddda5ef7fe75a21d0b978d3519133c
Сергей <sosnovskysy@yandex.ru>
2024-08-16 14:05:01 +0300

    optimization vol 3

--- src/main/java/dev/sosnovsky/parser/ParserApplication.java
+++ src/main/java/dev/sosnovsky/parser/ParserApplication.java
@@ -5,102 +5,101 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ParserApplication {
+    static int groupIndex = 1;
 
-    public static void main(String[] args) throws IOException {
+    // номер группы, список строк группы
+    static Map<Integer, Set<String>> groups = new HashMap<>();
+
+    // номер позиции слова, слово, номер группы
+    static Map<Integer, Map<String, Integer>> wordsWithPositionAndGroup = new HashMap<>();
+
+    public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Для запуска используйте команду: java -Xmx1G -jar Parser-1.0.jar <file_for_read>");
             return;
         }
 
-        String pathToFile = args[0];
-
-        // номер группы, список строк группы
-        Map<Integer, Set<String>> groups = new HashMap<>();
-
-        // номер позиции слова, слово, номер группы
-        Map<Integer, Map<String, Integer>> wordsWithPositionAndGroup = new HashMap<>();
-
-
         long startTime = System.currentTimeMillis();
+        String pathToFile = args[0];
 
         try (BufferedReader reader = new BufferedReader(new FileReader(pathToFile))) {
             String line;
-            int groupIndex = 1;
-
             while ((line = reader.readLine()) != null) {
-                int wordPosition = 0;
                 if (isValid(line)) {
-                    String[] words = line.split(";");
-                    Integer indexFirstGroup = null;
+                    parseLine(line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("\nОшибка чтения файла");
+            throw new RuntimeException(e);
+        }
 
-                    for (String word : words) {
-                        if (isNotEmpty(word)) {
-                            int currentGroup;
-                            if (wordsWithPositionAndGroup.containsKey(wordPosition)) {
-                                if (wordsWithPositionAndGroup.get(wordPosition).containsKey(word)) {
-                                    currentGroup = wordsWithPositionAndGroup.get(wordPosition).get(word);
-                                } else {
-                                    wordsWithPositionAndGroup.get(wordPosition).put(word, groupIndex);
-                                    currentGroup = groupIndex++;
-                                }
-                            } else {
-                                Map<String, Integer> newMap = new HashMap<>();
-                                newMap.put(word, groupIndex);
-                                wordsWithPositionAndGroup.put(wordPosition, newMap);
-                                currentGroup = groupIndex++;
-                            }
+        long countGroupWithMoreOneLine = groups.values().stream().filter(list -> list.size() >= 2).count();
 
-                            if (indexFirstGroup == null) {
-                                indexFirstGroup = currentGroup;
-                                if (groups.containsKey(indexFirstGroup)) {
-                                    Set<String> listFromGroup = groups.get(indexFirstGroup);
-                                    listFromGroup.add(line);
-                                    groups.put(indexFirstGroup, listFromGroup);
-                                } else {
-                                    Set<String> newSet = new HashSet<>(1);
-                                    newSet.add(line);
-                                    groups.put(indexFirstGroup, newSet);
-                                }
-                            } else if (indexFirstGroup != currentGroup) {
-                                Set<String> mainGroup = groups.get(indexFirstGroup);
-                                mainGroup.add(line);
-                                Set<String> mergeGroup;
-                                if (groups.get(currentGroup) != null) {
-                                    mergeGroup = groups.get(currentGroup);
-                                    mainGroup.addAll(mergeGroup);
-                                    for (String s : mergeGroup) {
-                                        String[] ws = s.split(";");
-                                        for (int i = 0; i < ws.length; i++) {
-                                            if (wordsWithPositionAndGroup.containsKey(i)) {
-                                                wordsWithPositionAndGroup.get(i).put(ws[i], indexFirstGroup);
-                                            }
-                                        }
-                                    }
-                                }
-                                wordsWithPositionAndGroup.get(wordPosition).put(words[wordPosition], indexFirstGroup);
-                                groups.put(indexFirstGroup, mainGroup);
-                                groups.remove(currentGroup);
-                            }
-                        }
-                        wordPosition++;
+        writeResultToFile(countGroupWithMoreOneLine);
+
+        System.out.println("\nКоличество групп с двумя и более строками: " + countGroupWithMoreOneLine);
+
+        System.out.println("Время выполнения составило " + (System.currentTimeMillis() - startTime) / 1000.0 + " сек\n");
+    }
+
+    private static void parseLine(String line) {
+        int wordPosition = 0;
+        String[] words = line.split(";");
+        Integer indexFirstGroup = null;
+
+        for (String word : words) {
+            if (isNotEmpty(word)) {
+                int currentGroup;
+                if (wordsWithPositionAndGroup.containsKey(wordPosition)) {
+                    if (wordsWithPositionAndGroup.get(wordPosition).containsKey(word)) {
+                        currentGroup = wordsWithPositionAndGroup.get(wordPosition).get(word);
+                    } else {
+                        wordsWithPositionAndGroup.get(wordPosition).put(word, groupIndex);
+                        currentGroup = groupIndex++;
+                    }
+                } else {
+                    Map<String, Integer> newMap = new HashMap<>();
+                    newMap.put(word, groupIndex);
+                    wordsWithPositionAndGroup.put(wordPosition, newMap);
+                    currentGroup = groupIndex++;
+                }
+
+                if (indexFirstGroup == null) {
+                    indexFirstGroup = currentGroup;
+                    groups.computeIfAbsent(indexFirstGroup, s -> new HashSet<>(1)).add(line);
+                } else if (indexFirstGroup != currentGroup) {
+                    mergeGroup(indexFirstGroup, currentGroup, line, wordPosition, words[wordPosition]);
+                }
+            }
+            wordPosition++;
+        }
+    }
+
+    private static void mergeGroup(int indexFirstGroup, int currentGroup, String line, int wordPosition, String word) {
+        Set<String> mainGroup = groups.get(indexFirstGroup);
+        mainGroup.add(line);
+        Set<String> mergeGroup;
+        if (groups.get(currentGroup) != null) {
+            mergeGroup = groups.get(currentGroup);
+            mainGroup.addAll(mergeGroup);
+            for (String s : mergeGroup) {
+                String[] ws = s.split(";");
+                for (int i = 0; i < ws.length; i++) {
+                    if (wordsWithPositionAndGroup.containsKey(i)) {
+                        wordsWithPositionAndGroup.get(i).put(ws[i], indexFirstGroup);
                     }
                 }
             }
         }
-
-        writeResultToFile(groups);
-
-        System.out.println("\nКоличество групп с двумя и более строками: "
-                + groups.values().stream().filter(list -> list.size() >= 2).count());
-
-        System.out.println("Время выполнения составило " + (System.currentTimeMillis() - startTime) / 1000.0 + " сек");
+        wordsWithPositionAndGroup.get(wordPosition).put(word, indexFirstGroup);
+        groups.put(indexFirstGroup, mainGroup);
+        groups.remove(currentGroup);
     }
 
-    //todo убрать скобки из вывода
-    private static void writeResultToFile(Map<Integer, Set<String>> groups) {
+    private static void writeResultToFile(long countGroupWithMoreOneLine) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.txt"))) {
-            writer.write("Количество групп с двумя и более строками: "
-                    + groups.values().stream().filter(list -> list.size() >= 2).count());
+            writer.write("Количество групп с двумя и более строками: " + countGroupWithMoreOneLine);
             writer.newLine();
 
             List<Set<String>> sortedList = groups.values()
